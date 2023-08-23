bac8d42dc4feee2914eeae00180fd06b31961713
Hariharan Elangovan <126451526+Hariharan15Elangovan@users.noreply.github.com>
2023-08-23 18:46:00 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,12 @@
+package org.cts;
+
+public class Sample {
+
+	private void hello() {
+		System.out.println("Hello world");
+	}
+	public static void main(String[] args) {
+		Sample s=new Sample();
+		s.hello();
+	}
+}
